f24d23030cc664c5317d770d4d7a5008c9f904df
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-03-31 15:06:20 -0300

    [init] - Inicia projeto

---
+++ SnapchatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatApp
+//
+//  Created by Jean Ramalho on 31/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
