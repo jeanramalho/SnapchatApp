a0632e6d7c4796f3ccefc73d9ad533e674855625
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-04-04 11:53:48 -0300

    [feat] - Implementa metodo novo de carregamento de imagem e coloca imagem escolhida na tela do novo snap

--- SnapchatApp/Controller/NovoSnapViewController.swift
+++ SnapchatApp/Controller/NovoSnapViewController.swift
@@ -6,18 +6,20 @@
 //
 import Foundation
 import UIKit
+import PhotosUI
 
-class NovoSnapViewController: UIViewController {
+class NovoSnapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let contentView: NovoSnapView = NovoSnapView()
     
     // Instancia variavel que controla selecao de imagens ou tirar foto
-    lazy var imagePicker: UIImagePickerController = {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
-        return imagePicker
-    }()
+    // Método antigo
+//    lazy var imagePicker: UIImagePickerController = {
+//        let imagePicker = UIImagePickerController()
+//        imagePicker.delegate = self
+//        imagePicker.sourceType = .photoLibrary
+//        return imagePicker
+//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,7 +62,7 @@ class NovoSnapViewController: UIViewController {
                     image: UIImage(systemName: "camera"),
                     style: .plain,
                     target: self,
-                    action: nil
+                    action: #selector(criarSnap)
                 )
             }
             
@@ -88,11 +90,49 @@ class NovoSnapViewController: UIViewController {
     }
     
     @objc private func criarSnap(){
+        // Metodo ultrapassado
+//        present(imagePicker, animated: true, completion: nil)
         
-        present(imagePicker, animated: true, completion: nil)
+        //MARK: Novo método para abrir a galeria
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 1 // Configura a quantidade de imagens a serem selecionadas
+        config.filter = .images // Pode usar .videos ou .any para os dois tipos
+        
+        let picker = PHPickerViewController(configuration: config) // instancia o imagePicker
+        picker.delegate = self
+        
+        present(picker, animated: true)
+        
     }
 }
 
-extension NovoSnapViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+extension NovoSnapViewController: PHPickerViewControllerDelegate {
+    
+    // metodo delegate chamado quando a usuário finaliza a selecao da foto
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        
+        // fecha o seletor de fotos apos a selecao
+        picker.dismiss(animated: true)
+        
+        // Verifica se existe uma selecao e pega a imagem selecionada
+        guard let selectedImg = results.first?.itemProvider,
+              selectedImg.canLoadObject(ofClass: UIImage.self) else {return}
+        
+        // Carrega o objeto como UIImage de forma assincrona
+        selectedImg.loadObject(ofClass: UIImage.self) { image, error in
+            
+            // Verifica se o objeto selecionado é uma imagem válida
+            if let imgSelected = image  as? UIImage {
+                
+                // Como vamos atualizar a interface isso deve ser feita de forma assincrona
+                DispatchQueue.main.async {
+                    
+                    // Utiliza a imagem
+                    self.contentView.snapImageView.image = imgSelected
+                }
+            }
+        }
+    }
+    
     
 }
